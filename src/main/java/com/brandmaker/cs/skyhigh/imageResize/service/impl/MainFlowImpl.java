86a5b12f979c48b66f5874d2ebb5277eb0b9bae3
miro <miroslav.patche@skyhighgrowth.com>
2020-10-29 10:32:12 +0100

    enable automatic scheduling 1AM every day

--- src/main/java/com/brandmaker/cs/skyhigh/imageResize/service/impl/MainFlowImpl.java
+++ src/main/java/com/brandmaker/cs/skyhigh/imageResize/service/impl/MainFlowImpl.java
@@ -2,6 +2,7 @@ package com.brandmaker.cs.skyhigh.imageResize.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.brandmaker.cs.skyhigh.imageResize.service.ImageResize;
@@ -15,7 +16,7 @@ public class MainFlowImpl implements MainFlow {
 	ImageResize imageResize;
 
 	@Override
-	//@Scheduled(cron = "0 0 1 * * *")
+	@Scheduled(cron = "0 0 1 * * *")
 	public void resizeScheduler() throws Exception {
 		System.out.println("working");
 		imageResize.runResize();
